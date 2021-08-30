e13b766a8144bfa8d07bd4392e3c6da570f91e13
Aman <amancoronel0397@gmail.com>
2021-08-30 12:39:21 +0800

    Add heroku link

--- src/middleware/app.middleware.ts
+++ src/middleware/app.middleware.ts
@@ -12,7 +12,7 @@ const Middleware = (app: Express) => {
     app.use(express.json());
     app.use(express.urlencoded({extended: true}))
     app.use(cors({
-        origin: ["http://localhost:3000", "*"],
+        origin: ["http://localhost:3000", "*", "https://prince-coronel-portfolio.herokuapp.com"],
         methods: ["GET", "PUT", "POST", "DELETE"],
         credentials: true
     }))
